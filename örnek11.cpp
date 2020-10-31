9b55c8fd49d3799af5e7deef401434f4d2e6c004
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-10-31 12:59:28 +0300

    Add files via upload

---
+++ örnek11.cpp
@@ -0,0 +1,18 @@
+//Rümeysa Coþkun
+// 1'den 100'e kadar olan sayýlarýn toplamýný bulan program.
+#include<iostream>
+using namespace std;
+int main()
+{
+int i=1;
+int t=0;
+for(i;i<100;i++)
+{
+t=t+i;
+}
+cout<<"Sum of the number:"<<endl;
+cout<<t;
+cout<<endl;
+system("PAUSE");
+return 0;
+}
